e9b562e990b3c66932e3a6f167973ed8c7b61fe0
ErdemBey31 <142883038+ErdemBey31@users.noreply.github.com>
2024-04-01 14:45:55 +0300

    Create index.py

---
+++ index.py
@@ -0,0 +1,24 @@
+import subprocess
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/command', methods=['POST'])
+def command():
+    user_command = request.form['command']
+    result = run_command(user_command)
+    return result
+
+def run_command(command):
+    try:
+        output = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT, universal_newlines=True)
+        return output
+    except subprocess.CalledProcessError as e:
+        return "Hata: " + str(e.output)
+
+if __name__ == '__main__':
+    app.run(debug=True)
