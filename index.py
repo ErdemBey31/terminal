72208ca17f42b6928201768d02b63cda66f6397a
ErdemBey31 <142883038+ErdemBey31@users.noreply.github.com>
2024-04-01 15:33:38 +0300

    Update index.py

--- index.py
+++ index.py
@@ -1,28 +1,14 @@
-import subprocess
-from flask import Flask, render_template, request
-from subprocess import PIPE, STDOUT
+import flask
+from flask_terminado import Terminal
 
-app = Flask(__name__)
+
+app = flask.Flask(__name__)
+
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def home():
+    return 'home'
 
-@app.route('/command', methods=['POST'])
-def command():
-    user_command = request.form['command']
-    result = run_command(user_command)
-    return result
 
-def run_command(command):
-    try:
-        process = subprocess.Popen(command, shell=True, stdout=PIPE, stderr=STDOUT, universal_newlines=True)
-        output = ''
-        while True:
-            line = process.stdout.readline()
-            if not line:
-                break
-            output += line
-        return output
-    except subprocess.CalledProcessError as e:
-        return "ERROR: " + str(e.output)
+terminal = Terminal(app)
+terminal.add_terminal('/bash', ['bash'])
